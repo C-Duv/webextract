5957ce29938177514ffd61d0c59d46cb1f4ec592
Gautier DI FOLCO <gautier.difolco@gmail.com>
2013-12-12 15:21:14 +0100

    Extraction en Ruby

---
+++ scripts/extract.rb
@@ -0,0 +1,38 @@
+require 'nokogiri'
+
+doc = Nokogiri::HTML(STDIN.read)
+
+props = {
+  "parProd"   => 'number(contains(parent::*/@class,"prod"))',
+  "ancProd"   => 'count(ancestor::*[contains(@class,"prod")])',
+  "ancDesc"   => 'count(ancestor::*[contains(@class,"desc")])',
+  "selfProd"  => 'number(contains(@class,"prod"))',
+  "selfDesc"  => 'number(contains(@class,"desc"))',
+  "selfCurr"  => 'number(contains(text(),"£") or contains(text(),"€")  or contains(text(),"$") or contains(text(), "EUR"))',
+  "selfDesc"  => 'number(contains(text(),"Desc") or contains(text(),"desc"))',
+  "selfEl"    => 'local-name(.)',
+  "parEl"     => 'local-name(..)',
+  "selfIP"    => '@itemprop',
+  "selfClass" => '@class',
+  "parClass"  => '../@class',
+  "selfDepth" => 'count(ancestor::*)',
+  "selfDesc"  => 'count(descendant::*)',
+  "inHn"      => 'ancestor-or-self::h1 or ancestor-or-self::h2 or ancestor-or-self::h3',
+}
+####
+# Search for nodes by xpath
+doc.xpath('/*/*/*/*//*[self::div or self::p or self::span or self::h1 or self::h2 or self::h3]').each do |e|
+    label = e.attr("data-tess-label")
+    line = []
+    if label
+        line << label
+    else
+        line << "other"
+    end
+
+    props.each do |name, exp|
+        line << name + "=" + e.xpath(exp).to_s
+    end
+
+    puts line.join(" ")
+end
