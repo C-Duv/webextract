472dfe655ab1bfd009db1ae3c059a75dc1e383e3
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-02-01 01:54:03 +0100

    Refresh features

--- dataset/11_raw_inst/tag.sh
+++
@@ -1,7 +0,0 @@
-for r in `ls | grep -v R | grep -v sh`
-do
-    for f in `ls $r`
-    do
-        echo "/home/gdifolco/ps11/crfsuite-0.12/bin/crfsuite tag -m /home/gdifolco/ps11/dataset/30_models/$r.mdl -t /home/gdifolco/ps11/dataset/11_raw_inst/$r/$f > /home/gdifolco/ps11/dataset/41_tags/$r/$f"
-    done
-done
---
+++ dataset/20_labeled_html/raw.sh
@@ -0,0 +1,7 @@
+for d in `ls | grep -v R | grep -v sh`; do
+    features=$(ruby ../../scripts/smart-extract.rb $d/* | tail -n 1)
+    for f in `ls ../10_raw_html/$d`; do
+        nf=${f#};
+        ruby ../../scripts/extract.rb $features < ../10_raw_html/$d/$f > ../11_raw_inst/${nf%.html} &
+    done
+done
