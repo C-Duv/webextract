e15595c28f95ed507acdde5f9e993118bbad96b9
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-02-04 00:27:08 +0100

    Tag correctly

---
+++ dataset/11_raw_inst/tag.sh
@@ -0,0 +1,7 @@
+for m in `ls | grep -v sh | grep -v RE`
+do
+    for f in `find "$m" -type f`
+    do
+        ../../../crfsuite/crfsuite-0.12/bin/crfsuite tag -m ../30_models/$m.mdl $f > ../41_tags/$f &
+    done
+done
