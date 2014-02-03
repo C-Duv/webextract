cca5a2521386f8ce03647b1a7936b518ddf714f8
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-02-03 10:58:53 +0100

    Update raw_inst

--- dataset/20_labeled_html/raw.sh
+++ dataset/20_labeled_html/raw.sh
@@ -2,6 +2,6 @@ for d in `ls | grep -v R | grep -v sh`; do
     features=$(ruby ../../scripts/smart-extract.rb $d/* | tail -n 1)
     for f in `ls ../10_raw_html/$d`; do
         nf=${f#};
-        ruby ../../scripts/extract.rb $features < ../10_raw_html/$d/$f > ../11_raw_inst/${nf%.html} &
+        ruby ../../scripts/extract.rb $features < ../10_raw_html/$d/$f > ../11_raw_inst/$d/${nf%.html} &
     done
 done
